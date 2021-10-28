515b237b42adde0ea02b6611540e2e6722cf72b8
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-10-28 06:59:21 -0400

    new test where user can change city

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -19,3 +19,13 @@ describe('<CitySearch /> component', () => {
         const query = CitySearchWrapper.state('query');
         expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
     });
+
+    test('change state when text input changes', () => {
+        const CitySearchWrapper = shallow(<CitySearch />);
+        CitySearchWrapper.setState({
+          query: 'Munich'
+        });
+        const eventObject = { target: { value: 'Berlin' }};
+        CitySearchWrapper.find('.city').simulate('change', eventObject);
+        expect(CitySearchWrapper.state('query')).toBe('Berlin');
+      });
