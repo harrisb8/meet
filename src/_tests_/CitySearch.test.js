b57e99de4e606eeefa2dd8f0909c970ce51aa063
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-10-28 06:49:19 -0400

    added new test for mockData

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -9,7 +9,13 @@ describe('<CitySearch /> component', () => {
     });
 });
 
-test('renders a list of suggestions', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
-    expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
-});
+    test('renders a list of suggestions', () => {
+        const CitySearchWrapper = shallow(<CitySearch />);
+        expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
+    });
+
+    test('renders text input correctly', () => {
+        const CitySearchWrapper = shallow(<CitySearch />);
+        const query = CitySearchWrapper.state('query');
+        expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
+    });
