aae705768e178630729c2dce23cd5ec49213345e
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-11-07 11:16:15 -0500

    add this.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     render() {
       return (
         <div className="App">
-          <CitySearch locations={this.state.locations} />
+          <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
           <EventList events={this.state.events} />
         </div>
       );
