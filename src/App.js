ec60263a18f42f665a51a216010c5e5df59d3319
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-11-07 10:11:07 -0500

    added state events

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <CitySearch />
-        <EventList />
-      </div>
-    );
-  } 
+  state = {
+    events: []
+  }
+
+  class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <CitySearch />
+          <EventList events={this.state.events} />
+        </div>
+      );
+    } 
+  }
 }
 export default App;
 
