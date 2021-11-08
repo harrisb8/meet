ca42b5330d05a51031167c147822957e10bb1a5b
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-11-08 07:45:02 -0500

    4.4 updates

--- src/api.js
+++ src/api.js
@@ -2,6 +2,16 @@ import { mockData } from './mock-data';
 import axios form 'axios';
 
 
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+    return result;
+};
+
 export const getEvents = async () => {
   return mockData;
 };
@@ -14,4 +24,20 @@ export const extractLocations = (events) => {
 
   export const getAccessToken = async () => {
     const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+      await localStorage.removeItem("access_token");
+      const seacrchParams = new URLSearchParams(window.location.search);
+      const code = await seacrchParams.get("code");
+      if (!code) {
+        const results = await axios.get(
+          "YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
+        );
+        const { authUrl } = results.data;
+        return (window.location.href = authUrl);
+      }
+      return code && getToken(code);
+    }
+      return accessToken;
   }
