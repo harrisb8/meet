12a759e090de4749f17975221d975c915921a2a9
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-11-10 20:06:08 -0500

    import nprogress css

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ import NProgress from 'nprogress';
 
 
 
+
 const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
