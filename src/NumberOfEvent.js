b3a5819b4697a356925b18edf60624db56a5124c
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-11-27 18:45:57 -0500

    Update error text

--- src/NumberOfEvent.js
+++ src/NumberOfEvent.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import { ErrorAlert } from './Alert';
 
 class numberOfEvent extends Component {
-    changeHandler= (e) => {
-        this.props.changeNumberOfEvents(undefined, e.target.value);
-    }
+    state = {
+        numberOfEvent: 32,
+    //changeHandler= (e) => {
+      //  this.props.changeNumberOfEvents(undefined, e.target.value);
+    };
+
+    handleInputChange = (event) => {
+        const value = event.target.value;
+        this.props.updateEvents(null, value);
+        this.setState({ numberOfEvent: value });
+
+        if (value < 1) {
+            this.setState({
+                errorText: "Select number from 1 to 32",
+            });
+        }else {
+            this.setState({
+                infoText: "",
+            });
+        }
+    };
 
     render() {
         return ( 
             <div className= "numberOfEvent">
                 <h3>Number of Events</h3>
-                <input className='events' value={this.props.numberOfEvents} type='number' onChange={this.changeHandler}/>
+                <input className='events' value={this.props.numberOfEvents} type='number' onChange={this.handleInputChanged}/>
                 <ErrorAlert text={this.state.errorText} />
             </div>
         );
