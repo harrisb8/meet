b019a7cd1be637754ee920c05c732fe9d6b81508
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-11-27 17:39:17 -0500

    added infotext

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -12,14 +12,23 @@ class CitySearch extends Component {
     
       handleInputChanged = (event) => {
         const value = event.target.value;
+        this.setState({showSuggestions:true});
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({
+        if (suggestions.length === 0) {
+          this.setState({
              query: value,
-             suggestions,
+             infoText: 'We can not find the city you are looking for. Please try another city',
             });
-      };
+      } else {
+          return this.setState({
+              query: value,
+              suggestions,
+              infoText:''
+          });
+      }
+    };
 
       handleItemClicked = (suggestion) => {
           this.setState({
