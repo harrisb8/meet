6f95cbb8bcb2489747c59b72938322d69703d924
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2022-01-15 18:40:52 -0500

    update graph

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import NumberOfEvent from './NumberOfEvent';
 import { extractLocations, getEvents } from './api';
 import "./nprogress.css";
 import {
-  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
 } from 'recharts';
+
 
 class App extends Component {
   state = {
@@ -68,19 +69,19 @@ class App extends Component {
                
         <h4>Events in each city</h4>
 
-        <ScatterChart
-          width={400}
-          height={400}
+        <ResponsiveContainer height={400} >
+        <ScatterChart 
           margin={{
           top: 20, right: 20, bottom: 20, left: 20,
           }}
         >
       <CartesianGrid />
-          <XAxis type="category" dataKey="city" name="city" unit="cm" />
-          <YAxis type="number" dataKey="number" name="number of events" unit="kg" />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis allowDecimals={false} type="number" dataKey="number" name="number of events" />
           <Tooltip cursor={{ strokeDasharray: '3 3' }} />
           <Scatter data={this.getData()} fill="#8884d8" />
           </ScatterChart>
+          </ResponsiveContainer>
           <EventList events={this.state.events} />
       </div>
     );
