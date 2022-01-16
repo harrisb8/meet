b7f18a8a0a2fbb4601f5d9e40837c83ca11bf977
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2022-01-16 13:51:55 -0500

    fixed graph error

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class App extends Component {
                
         <h4>Events in each city</h4>
         <div className="data-vis-wrapper">
-          <EventGenre events={events} />
+          <EventGenre events={this.state.events} />
           <ResponsiveContainer height={400} >
             <ScatterChart 
               margin={{
