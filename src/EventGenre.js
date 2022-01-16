713a8a4c4a2f9e1a7e1be02534a2784da3045fce
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2022-01-16 13:46:56 -0500

    update pie chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -4,12 +4,19 @@ import {
 } from 'recharts';
 
 const EventGenre = ({ events }) => {
-    const data = [
-        { name: 'Group A', value 400 },
-        { name: 'Group B', value 300 },
-        { name: 'Group C', value 300 },
-        { name: 'Group D', value 200 },
-    ];
+    const [data, setData] = useState([]);
+    useEffect(() => { setData(() => getData()); }, [events]);
+
+    const getData = () => {
+        const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+        const data = genres.map((genre)=>{
+            const value = events.filter((event) => event.summary.split(' ').includes(genre)).length; 
+            return { name: genre, value }; 
+        });
+        return data;
+    };
+
+
     return (
         <ResponsiveContainer height={400} >
             <PieChart width={400} height={400}>
