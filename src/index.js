2aeebbe7787c3c4e454578eb829bc470436df447
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2022-01-24 16:47:04 -0500

    update CF 4.8

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-//import * as atatus from 'status-spa';
+
 
 
 
